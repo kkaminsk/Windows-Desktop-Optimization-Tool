dbdb6468328e020e953ba833f87004e9a45c9370
Robert Smith <61853161+robsmi-msfte@users.noreply.github.com>
2025-11-25 18:26:34 -0500

    Update OneDrive removal script to exclude WinSxS
    Also, be more specific about what actually does get deleted. This fix is because of an issue where if "OneDrive.svg" gets deleted, Windows 11 will fail the installation of the monthly CU, and the log shows it's because the .svg file is missing.
    This change now only attempts to remove OneDrive folder, OneDrive.ico, and OneDrive.exe. The fix for this has already been tested and confirmed by another project.

--- Functions/Remove-WDOTRemoveOneDrive.ps1
+++ Functions/Remove-WDOTRemoveOneDrive.ps1
@@ -26,7 +26,7 @@ function Remove-WDOTRemoveOneDrive
         }
 
         Write-EventLog -EventId 80 -Message "Removing shortcut links for OneDrive" -LogName 'WDOT' -Source 'AdvancedOptimizations' -EntryType Information
-        Get-ChildItem 'C:\*' -Recurse -Force -EA SilentlyContinue -Include 'OneDrive', 'OneDrive.*' | Remove-Item -Force -Recurse -EA SilentlyContinue
+        Get-ChildItem 'C:\*' -Recurse -Force -EA SilentlyContinue -Include 'OneDrive', 'OneDrive.exe', 'OneDrive.ico' -Exclude 'C:\Windows\WinSxS' | Remove-Item -Force -Recurse -EA SilentlyContinue
 
     }
     End
